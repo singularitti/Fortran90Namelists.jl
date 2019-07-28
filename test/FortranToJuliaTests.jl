8cfb4157170cba7ae63dfe21bad55df4b74e7368
Qi Zhang <singularitti@outlook.com>
2019-07-28 04:44:01 -0400

    test: Deprecate tests for `guesstype`

--- test/FortranToJuliaTests.jl
+++ test/FortranToJuliaTests.jl
@@ -14,44 +14,6 @@ using Fortran90Namelists.FortranToJulia
 
 # Test data referenced from: http://www-classes.usc.edu/engr/ce/108/text/fbk01.htm.
 
-# @testset "Test `guesstype`" begin
-#     @test guesstype(f"1.0E-6") == Float32
-#     @test guesstype(f"3.2767e+2") == Float32
-#     @test guesstype(f"1.89e-14") == Float32
-#     @test guesstype(f"-0.65e-2") == Float32
-#     @test guesstype(f"+1e8") == Float32
-#
-#     @test guesstype(f"1.0D-6") == Float64
-#     @test guesstype(f"1d8") == Float64
-#     @test guesstype(f"0.") == Float64
-#     @test guesstype(f"1.00") == Float64
-#     @test guesstype(f"+3.141593") == Float64
-#     @test guesstype(f"+3.1415926535d+0") == Float64
-#     @test guesstype(f"-4.78d+6") == Float64
-#     @test guesstype(f"1.0d+0") == Float64
-#
-#     @test guesstype(f"(5.229, -4.78)") == Complex{Float64}
-#     @test guesstype(f"(0.0,1.0)") == Complex{Float64}
-#     @test guesstype(f"(0.0,1)") == Complex{Real}
-#     @test guesstype(f"(3.2767e+2, -0.65e-2)") == Complex{Float32}
-#
-#     @test guesstype(f"124") == Integer
-#     @test guesstype(f"-448") == Integer
-#     @test guesstype(f"0") == Integer
-#     @test guesstype(f"32767") == Integer
-#     @test guesstype(f"2147483647") == Integer
-#     @test guesstype(f"-9874") == Integer
-#
-#     @test guesstype(f".true.") == Bool
-#     @test guesstype(f".t.") == Bool
-#     @test guesstype(f".false.") == Bool
-#     @test guesstype(f".f.") == Bool
-#
-#     @test guesstype(f"''") == String
-#     @test guesstype(f"\"./tmp234\"") == String
-#     @test guesstype(f"'david'") == String
-# end  # testset
-
 @testset "Test `parse*` functions" begin
     @test parse(Float32, f"1.0E-6") === 1.0f-6
     @test parse(Float32, f"3.2767e+2") === 3.2767f2
