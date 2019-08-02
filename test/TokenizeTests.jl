179146d2ffe406a0190022c21bfb00228ea92d54
Qi Zhang <singularitti@outlook.com>
2019-08-02 10:28:20 -0400

    Add tests for string.nml

--- test/TokenizeTests.jl
+++ test/TokenizeTests.jl
@@ -10,26 +10,26 @@ using Test
 
 using Fortran90Namelists.Tokenize
 
-# @testset "" begin
-#     tk = Tokenizer()
-#     benchmark = [["&", "string_nml"],
-#     ["    ", "str_basic", " ", "=", " ", "\"hello\""],
-#     ["    ", "str_no_delim", " ", "=", " ", "hello"],
-#     ["    ", "str_no_delim_no_esc", " ", "=", " ", "a\"\"b"],
-#     ["    ", "single_esc_delim", " ", "=", " ", "\"a \"\"single\"\" delimiter\""],
-#     ["    ", "double_esc_delim", " ", "=", " ", "\"a \"\"double\"\" delimiter\""],
-#     ["    ", "double_nested", " ", "=", " ", "\"\"\"x\"\" \"\"y\"\"\""],
-#     ["    ", "str_list", " ", "=", " ", "\"a\"", ",", " ", "\"b\"", ",", " ", "\"c\""],
-#     ["    ", "slist_no_space", " ", "=", " ", "\"a\"", ",", "\"b\"", ",", "\"c\""],
-#     ["    ", "slist_no_quote", " ", "=", " ", "a", ",", "b", ",", "c"],
-#     ["    ", "slash", " ", "=", " ", "\"back\\slash\""],
-#     ["/"]]
-#     open("test/data/string.nml", "r") do io
-#         for (i, line) in enumerate(eachline(io))
-#             @test parse(tk, line) == benchmark[i]
-#         end
-#     end
-# end # testset
+@testset "Test string" begin
+    tk = Tokenizer()
+    benchmark = [["&", "string_nml"],
+    ["    ", "str_basic", " ", "=", " ", "'hello'"],
+    ["    ", "str_no_delim", " ", "=", " ", "hello"],
+    ["    ", "str_no_delim_no_esc", " ", "=", " ", "a''b"],
+    ["    ", "single_esc_delim", " ", "=", " ", "'a ''single'' delimiter'"],
+    ["    ", "double_esc_delim", " ", "=", " ", "\"a \"\"double\"\" delimiter\""],
+    ["    ", "double_nested", " ", "=", " ", "\"''x'' \"\"y\"\"\""],
+    ["    ", "str_list", " ", "=", " ", "'a'", ",", " ", "'b'", ",", " ", "'c'"],
+    ["    ", "slist_no_space", " ", "=", " ", "'a'", ",", "'b'", ",", "'c'"],
+    ["    ", "slist_no_quote", " ", "=", " ", "a", ",", "b", ",", "c"],
+    ["    ", "slash", " ", "=", " ", "'back\\slash'"],
+    ["/"]]
+    open("test/data/string.nml", "r") do io
+        for (i, line) in enumerate(eachline(io))
+            @test parse(tk, line) == benchmark[i]
+        end
+    end
+end # testset
 
 @testset "Test dollar" begin
     benchmark = [[raw"$", "dollar_nml"],
