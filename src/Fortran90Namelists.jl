cc895b01ce8fce808f038b752bc0275bf02ae032
singularitti <singularitti@outlook.com>
2023-07-14 21:50:49 -0400

    Rename src/JuliaToFortran.jl to src/fstring.jl

--- src/Fortran90Namelists.jl
+++ src/Fortran90Namelists.jl
@@ -1,7 +1,7 @@
 module Fortran90Namelists
 
 include("fparse.jl")
-include("JuliaToFortran.jl")  # The order has to be like this!
+include("fstring.jl")  # The order has to be like this!
 include("Tokenize.jl")
 
 end # module
