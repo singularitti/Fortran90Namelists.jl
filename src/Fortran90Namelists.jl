9fcfc0335d297f5443342a5cd9d9967951ec3442
singularitti <singularitti@outlook.com>
2023-07-17 02:52:26 -0400

    Rename Lexer.jl to lex!.jl

--- src/Fortran90Namelists.jl
+++ src/Fortran90Namelists.jl
@@ -3,6 +3,6 @@ module Fortran90Namelists
 include("fparse.jl")
 include("fstring.jl")  # The order has to be like this!
 include("Tokenizer.jl")
-include("Lexer.jl")
+include("lex!.jl")
 
 end # module
